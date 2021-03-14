30793799b39a857d6cec7ff39eb06de6c6fbf6bb
Adryane Fernandes <edryfernandes@gmail.com>
2021-03-14 00:11:10 -0300

    Adições extras: Rolagem, mensagem começando de baixo pra cima e estilização dos balões

--- whatslab/src/components/Mensagens.js
+++ whatslab/src/components/Mensagens.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import styled from "styled-components";
 
+//Pro nome ficar em cima
+const Nome = styled.span `
+    color: #f66800;
+    font-size: 0.8rem;
+    font-weight: 500;
+    margin-bottom: 0.2rem;
+`
+const ContainerMsg = styled.div `
+    display: flex;
+    flex-direction: column;
+`
+
 function Mensagens(props) {
     return (
-        <div>
-            <b>{props.nomeUsuario}{props.doisPontos}</b> 
+        <ContainerMsg>
+            <Nome>{props.nomeUsuario}</Nome> 
             <span> {props.msgUsuario}</span>
-        </div>
+        </ContainerMsg>
     );
 };
 
